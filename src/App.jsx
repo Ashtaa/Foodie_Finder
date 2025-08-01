410afeb42ad17a19e44da331529b583542680570
Nahom Gebre <nahomgebre73@gmail.com>
2025-08-01 21:20:10 +0300

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Catagory from './pages/Catagory';
 import MealDetail from './pages/MealDetail';
+import Random from './pages/Random';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/catagory" element={<Catagory />} />
       <Route path="/catagory/:name" element={<Catagory />} />
       <Route path="/meal/:id" element={<MealDetail />} />
+      <Route path="/random" element={<Random />} />
     </Routes>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Catagory from './Catagory';
+import Random from './Random';
 
-function Home() {
+const Home = () => {
   return (
-    <div>home</div>
-  )
-}
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Welcome to Foodie Finder 🍽️</h1>
+      
+      <section className="mb-8">
+        <Random />
+      </section>
 
-export default Home
+      <section>
+        <Catagory />
+      </section>
+    </div>
+  );
+};
+
+export default Home;
