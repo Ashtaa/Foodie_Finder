31914e816cf990c206fa575d0c519c39747098b6
Zufan-Hadgu <zufanhadgu1@gmail.com>
2025-08-02 12:10:47 +0300

    Updated serach result page

--- src/pages/SearchedResult.jsx
+++ src/pages/SearchedResult.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import MealDetail from './MealDetail';
- // Make sure this import path is correct
+ 
 
 function SearchedResult() {
   const [meals, setMeals] = useState([]);
